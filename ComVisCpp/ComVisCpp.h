f7ca2be15b34eef8a50287964dc58d0a3cc580e9
joramwessels <joram.wessels@outlook.com>
2019-02-17 17:42:34 +0100

    Explicit Rt shit

--- ComVisCpp/ComVisCpp.h
+++ ComVisCpp/ComVisCpp.h
@@ -12,6 +12,8 @@ struct Extrinsics
 	cv::Mat t; // translation vector
 };
 
+bool explicitImplementation = true;
+
 void createKnownBoardPosition(cv::Size boardSize, float squareEdgeLength, std::vector<cv::Point3f>& corners);
 void getChessboardCorners(std::vector<cv::Mat> images, cv::Size boardSize, std::vector<std::vector<cv::Point2f>>& allFoundPoints, bool showResults = false);
 void cameraCalibration(std::vector<cv::Mat> calibrationImages, cv::Size boardSize, float squareEdgeLength, cv::Mat& cameraMatrix, cv::Mat& distortionCoefficients, std::vector<cv::Mat>& rvecs, std::vector<cv::Mat>& tvecs, bool showResults = false);
@@ -21,3 +23,5 @@ void drawAxes(cv::Mat &inputImage, cv::Mat rvecs, cv::Mat tvecs, cv::Mat cameraM
 void drawAxesManually(cv::Mat K, std::vector<cv::Mat> rvecs, std::vector<cv::Mat> tvecs, cv::Mat img, cv::Size boardDim, float cellSize);
 cv::Mat rotationVectorToMatrix(cv::Mat rvecs);
 cv::Mat makeTransformationMatrix(cv::Mat R, cv::Mat t);
+
+void printOpenCVMatrix(cv::Mat m);
