d87a33a7d3cacaccf20b9bc3101243520f65fc67
joramwessels <joram.wessels@outlook.com>
2019-02-17 15:41:14 +0100

    Reorganized

--- ComVisCpp/ComVisCpp.h
+++ ComVisCpp/ComVisCpp.h
@@ -1,10 +1,10 @@
 #pragma once
 
-void createKnownBoardPosition(Size boardSize, float squareEdgeLength, vector<Point3f>& corners);
-void getChessboardCorners(vector<Mat> images, Size boardSize, vector<vector<Point2f>>& allFoundPoints, bool showResults = false);
-void cameraCalibration(vector<Mat> calibrationImages, Size boardSize, float squareEdgeLength, Mat& cameraMatrix, Mat& distortionCoefficients, vector<Mat>& rvecs, vector<Mat>& tvecs, bool showResults = false);
-void printMatrix(Mat matrix, string header = "");
-void drawAxes(Mat &inputImage, Mat rvecs, Mat tvecs, Mat cameraMatrix, Mat distMatrix);
+void createKnownBoardPosition(cv::Size boardSize, float squareEdgeLength, std::vector<cv::Point3f>& corners);
+void getChessboardCorners(std::vector<cv::Mat> images, cv::Size boardSize, std::vector<std::vector<cv::Point2f>>& allFoundPoints, bool showResults = false);
+void cameraCalibration(std::vector<cv::Mat> calibrationImages, cv::Size boardSize, float squareEdgeLength, cv::Mat& cameraMatrix, cv::Mat& distortionCoefficients, std::vector<cv::Mat>& rvecs, std::vector<cv::Mat>& tvecs, bool showResults = false);
+void printMatrix(cv::Mat matrix, std::string header = "");
+void drawAxes(cv::Mat &inputImage, cv::Mat rvecs, cv::Mat tvecs, cv::Mat cameraMatrix, cv::Mat distMatrix);
 
 void drawAxesManually(cv::Mat K, std::vector<cv::Mat> rvecs, std::vector<cv::Mat> tvecs, cv::Mat img);
 cv::Mat rotationVectorToMatrix(cv::Mat rvecs);
